5a552a5c17f3f3f6a57ba08eefd0ced2356a8374
asimfayyazexe <asimfayyaz109@gmail.com>
2026-04-15 13:50:10 +0500

    Improved validation and UI fixes

---
+++ script.js
@@ -0,0 +1,40 @@
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let emailError = document.querySelector(".emailError");
+  let passwordError = document.querySelector(".passwordError");
+
+  emailError.textContent = "";
+  passwordError.textContent = "";
+
+  let isValid = true;
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+
+  let emailAns = emailRegex.test(email.value);
+  let passwordAns = passwordRegex.test(password.value);
+
+  console.log(emailAns, passwordAns); 
+
+  if (!emailAns) {
+    emailError.textContent = "Invalid email address";
+    emailError.style.display = "initial";
+    isValid = false;
+  }
+
+  if (!passwordAns) {
+    passwordError.textContent = "Password must be strong";
+    passwordError.style.display = "initial";
+    isValid = false;
+  }
+
+  if (isValid) {
+    console.log("Form Submitted");
+    // form.submit(); // optional
+  }
+});
